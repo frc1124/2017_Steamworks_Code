24995d214910098b0825333d03e4ef4a11e163fe
walnutwaldo <walnutwaldo20@gmail.com>
2017-01-12 19:06:14 -0500

    Added Requires to Arcade

--- Uberbots2017/src/org/usfirst/frc/team1124/robot/commands/TeleopArcade.java
+++ Uberbots2017/src/org/usfirst/frc/team1124/robot/commands/TeleopArcade.java
@@ -1,32 +1,15 @@
 package org.usfirst.frc.team1124.robot.commands;
 
-import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.command.Command;
 
 import org.usfirst.frc.team1124.robot.OI;
 import org.usfirst.frc.team1124.robot.Robot;
-import org.usfirst.frc.team1124.robot.RobotMap;
 
 public class TeleopArcade extends Command {
-	private PWM wheelOne;
-	private PWM wheelTwo;
-	private PWM wheelThree;
-	private PWM wheelFour;
+	
+	public TeleopArcade() {requires(Robot.drive);}
 
-	private double x;
-	private double y;
-
-	public TeleopArcade() {
-		wheelOne = new PWM(RobotMap.FRONT_LEFT);
-		wheelTwo = new PWM(RobotMap.FRONT_RIGHT);
-		wheelThree = new PWM(RobotMap.BACK_LEFT);
-		wheelFour = new PWM(RobotMap.BACK_RIGHT);
-	}
-
-	protected void execute() {
-		
-		Robot.drive.getRobotDrive().arcadeDrive(OI.stick);
-	}
+	protected void execute() {Robot.drive.getRobotDrive().arcadeDrive(OI.stick);}
 
 	protected boolean isFinished() {
 		return (false);
