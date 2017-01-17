3b35f2c84721a8e8b1dba07b460081cf35ba57cb
walnutwaldo <walnutwaldo20@gmail.com>
2017-01-16 19:22:48 -0500

    IDs

--- Uberbots2017/src/org/usfirst/frc/team1124/robot/RobotMap.java
+++ Uberbots2017/src/org/usfirst/frc/team1124/robot/RobotMap.java
@@ -3,10 +3,10 @@ package org.usfirst.frc.team1124.robot;
 public class RobotMap {
 
 	// even on left, low in front
-	public static final int FRONT_LEFT = 0;
-	public static final int FRONT_RIGHT = 1;
-	public static final int BACK_LEFT = 2;
-	public static final int BACK_RIGHT = 3;
+	public static final int FRONT_LEFT = 1;
+	public static final int FRONT_RIGHT = 2;
+	public static final int BACK_LEFT = 3;
+	public static final int BACK_RIGHT = 4;
 
 	public static final int[] A_CHANNEL = { 0, 2, 4, 6 };
 	public static final int[] B_CHANNEL = { 1, 3, 5, 7 };
