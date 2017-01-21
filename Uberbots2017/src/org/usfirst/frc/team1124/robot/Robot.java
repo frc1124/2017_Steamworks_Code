c7eca4e36dd5e66037a60cbdae1362b609a0d483
Chris Barnes <chris.barnes@uberbots.org>
2017-01-21 16:29:38 -0500

    Camera Update
    Working on the Camera

--- Uberbots2017/src/org/usfirst/frc/team1124/robot/Robot.java
+++ Uberbots2017/src/org/usfirst/frc/team1124/robot/Robot.java
@@ -2,20 +2,24 @@
 package org.usfirst.frc.team1124.robot;
 
 import org.usfirst.frc.team1124.robot.subsystems.Drive;
+import org.usfirst.frc.team1124.vision.*;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 
+
 public class Robot extends IterativeRobot {
 	public static Command teleopDrive;
 	public static OI oi;
 	public static Drive drive;
+	public static Camera camera1;
 	public void robotInit() {
 		RobotMap.init();
 
 		drive = new Drive();
 		oi = new OI();
+		camera1 = new Camera();
 	}
 
 	public void disabledInit() {}
