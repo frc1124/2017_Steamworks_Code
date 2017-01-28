35f0624f02e59a6424e2c3f80f01c850e1854c30
Chris Barnes <chris.barnes@uberbots.org>
2017-01-27 20:48:57 -0500

    Target Filter
    Set up target Filter.
    Still needs some work.

--- Uberbots2017/src/org/usfirst/frc/team1124/robot/RobotMap.java
+++ Uberbots2017/src/org/usfirst/frc/team1124/robot/RobotMap.java
@@ -7,5 +7,8 @@ public class RobotMap {
 	public static final int FRONT_RIGHT = 3;
 	public static final int BACK_RIGHT = 4;
 
+	public static final int CAMERA1_RES_X = 640;
+	public static final int CAMERA1_RES_Y = 480;
+	
 	public static void init() {}
 }
