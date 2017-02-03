4f0bba4de8bcbfda94324123baaae151214f83aa
Shmish <shmish90@gmail.com>
2017-02-02 20:41:14 -0500

    fix

--- Uberbots2017/src/org/usfirst/frc/team1124/robot/Robot.java
+++ Uberbots2017/src/org/usfirst/frc/team1124/robot/Robot.java
@@ -1,21 +1,14 @@
 package org.usfirst.frc.team1124.robot;
 
-<<<<<<< HEAD
 import org.usfirst.frc.team1124.robot.commands.Auto;
 import org.usfirst.frc.team1124.robot.commands.Teleop;
 import org.usfirst.frc.team1124.robot.subsystems.Drive;
 import org.usfirst.frc.team1124.robot.subsystems.Pneumatics;
-=======
-import org.usfirst.frc.team1124.robot.commands.TeleopDrive;
-import org.usfirst.frc.team1124.robot.subsystems.Drive;
-import org.usfirst.frc.team1124.vision.Camera;
-
->>>>>>> master
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 
 public class Robot extends IterativeRobot {
-<<<<<<< HEAD
     public static Drive drive = new Drive();
     public static Pneumatics pneumatics = new Pneumatics();
     public static OI oi = new OI();
@@ -29,37 +22,6 @@ public class Robot extends IterativeRobot {
 
     public void disabledInit() {}
 	public void autonomousInit() { auto.start(); }
-=======
-
-	// Operator Interface
-	public static OI oi;
-
-	// Subsystems
-	public static Drive drive;
-	public static Camera camera1;
-
-	// Commands
-	public static TeleopDrive teleopDrive;
-
-	public void robotInit() {
-
-		oi = new OI();
-
-		drive = new Drive();
-		camera1 = new Camera();
-
-		teleopDrive = new TeleopDrive();
-
-		// reseting the NavX
-		drive.getNavx().reset();
-		drive.getNavx().zeroYaw();
-	}
-
-	public void disabledInit() {}
-
-	public void autonomousInit() {}
-
->>>>>>> master
 	public void teleopInit() {}
 	public void testInit() {}
 
