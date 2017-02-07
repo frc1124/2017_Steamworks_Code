277b4d9986d36f86ab10ec34bf4c5d64d06ab886
Shmish <shmish90@gmail.com>
2017-02-07 15:34:32 -0500

    fixed climber

--- Uberbots2017/src/org/usfirst/frc/team1124/robot/subsystems/Climber.java
+++ Uberbots2017/src/org/usfirst/frc/team1124/robot/subsystems/Climber.java
@@ -1,14 +1,15 @@
 package org.usfirst.frc.team1124.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Climber extends Subsystem {
-	private Solenoid ropeDoor = new Solenoid(0, 1);
+	private DoubleSolenoid ropeDoor = new DoubleSolenoid(0, 2, 3);
 	
 	public Climber() {}
 	
     public void initDefaultCommand() {}
-    public void toggle() { ropeDoor.set(!ropeDoor.get()); }
+    public void toggle() { ropeDoor.set((ropeDoor.get()==Value.kForward) ? Value.kReverse : Value.kForward); }
 }
 
