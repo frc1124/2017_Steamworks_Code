c965ff57d5f3de5804c120e50a1dc496f16f8fb9
Jennifer Weston <jweston@uberbots.org>
2017-02-20 18:09:55 -0500

    Silly attempt at strafe cmd based ripped from DriveForward

--- Uberbots2017/src/org/usfirst/frc/team1124/robot/commands/Strafe.java
+++ Uberbots2017/src/org/usfirst/frc/team1124/robot/commands/Strafe.java
@@ -1,50 +1,131 @@
 package org.usfirst.frc.team1124.robot.commands;
 
 import org.usfirst.frc.team1124.robot.Robot;
-
+import org.usfirst.frc.team1124.robot.subsystems.Drive;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class Strafe extends Command {
 	
-	private double time = 0;
+	@SuppressWarnings("unused")
 	private double distance;
-	private double curDistance = 0;
-	
-	private double speed = 0.4;
-	
-	private long deltaTime = 0;
+	private double distanceInTicks;
+
+	private boolean done = false;
+
+	private static final double MAX_SPEED = 0.9;
+
+	private static final double DISTANCE_PER_TICK = 4 * Math.PI / 4096;
+
+	private static final int TICKS_TIL_FULL = 16000;
+
+	private int sign;
+
+	private int frontLeftStart;
+	private int frontRightStart;
+	private int rearLeftStart;
+	private int rearRightStart;
 	
 	public Strafe(double distance){
 		this.distance = distance;
-	}
-	
-	public Strafe(double distance, double speed){
-		this.distance = distance;
-		this.speed = speed;
+		sign = (int) (distance / Math.abs(distance));
+		// Need to go twice as many ticks because the wheels are working at odds, diminishing in power by 1/2
+		this.distanceInTicks = 2 * sign * distance / DISTANCE_PER_TICK;
+		requires(Robot.drive);
 	}
 
-	@Override
-	protected void interrupted(){
-		super.interrupted();
-	}
-	
-	@Override
 	protected void initialize() {
-		super.initialize();
+		frontLeftStart = Robot.drive.frontLeft.getEncPosition();
+		frontRightStart = Robot.drive.frontRight.getEncPosition();
+		rearLeftStart = Robot.drive.rearLeft.getEncPosition();
+		rearRightStart = Robot.drive.rearRight.getEncPosition();
+		done = false;
 	}
 
-	@Override
 	protected void execute() {
-		long time1 = System.nanoTime();
-		super.execute();
-		curDistance += Robot.speedX() * deltaTime;
-		deltaTime = System.nanoTime() - time1;
-		
+		Drive drive = Robot.drive;
+
+		/*
+		 * going right:
+		 * changeFL +
+		 * changeRL -
+		 * changeFR -
+		 * changeRR +
+		 * 
+		 * going left:
+		 * changeFL -
+		 * changeRL +
+		 * changeFR +
+		 * changeRL -
+		 * 
+		 * sign left -
+		 * sign right +
+		 */
+
+		int changeFR = Math.abs(drive.frontRight.getEncPosition() - frontRightStart);
+		int changeFL = Math.abs(drive.frontLeft.getEncPosition() - frontLeftStart);
+		int changeRR = Math.abs(drive.rearRight.getEncPosition() - rearRightStart);
+		int changeRL = Math.abs(drive.rearLeft.getEncPosition() - rearLeftStart);
+
+		int average = sign * (changeFR + changeRR + changeFL + changeRL) / 4;
+		double speed = sign * getSpeed(average);
+		if (Math.abs(distanceInTicks - average) < 3000)
+			quit();
+		else {
+			drive.mode = 2;
+			drive.driveTrain.mecanumDrive_Cartesian(speed,0, drive.turnController.getOutput(closeToLockAngle(Robot.drive.navx.getYaw()), drive.lockAngle), 0);
+		}
+
+		NetworkTable.getTable("encoders").putNumber("average", average);
+		NetworkTable.getTable("encoders").putNumber("trySpeed", speed);
+		NetworkTable.getTable("encoders").putNumber("distanceInTicks", distanceInTicks);
 	}
 
-	@Override
-	protected boolean isFinished() {
-		// TODO Auto-generated method stub
-		return curDistance >= distance;
+	private double closeToLockAngle(double yaw) {
+		double yaw2 = yaw + 360;
+		double yaw3 = yaw - 360;
+		if (Math.abs(yaw3 - Robot.drive.lockAngle) < Math.abs(yaw2 - Robot.drive.lockAngle)) {
+			yaw2 = yaw3;
+		}
+		if (Math.abs(yaw2 - Robot.drive.lockAngle) < Math.abs(yaw - Robot.drive.lockAngle)) {
+			yaw = yaw2;
+		}
+		return yaw;
 	}
+
+	private void quit() {
+		Drive drive = Robot.drive;
+		drive.frontLeft.set(0);
+		drive.frontRight.set(0);
+		drive.rearLeft.set(0);
+		drive.rearRight.set(0);
+		done = true;
+	}
+
+	private double getSpeed(int ticksSoFar) {
+		if (ticksSoFar < distanceInTicks / 2) {
+			if (ticksSoFar >= TICKS_TIL_FULL)
+				return MAX_SPEED;
+			else
+				return Math.max(0.3, MAX_SPEED / TICKS_TIL_FULL * ticksSoFar);
+		} else {
+			if (ticksSoFar >= distanceInTicks - TICKS_TIL_FULL)
+				return MAX_SPEED / TICKS_TIL_FULL * (distanceInTicks - ticksSoFar);
+			else
+				return MAX_SPEED;
+		}
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return done;
+	}
+
+	public boolean isRunning() {
+		return !done;
+	}
+
+	protected void end() {}
+
+	protected void interrupted() { this.end(); }
 }
