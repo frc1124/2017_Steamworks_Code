367168717c7ed5bef7756f3880f8f2970b61b598
Jennifer Weston <jweston@uberbots.org>
2017-02-23 10:32:18 -0500

    Add ParkAtFeeder

--- Uberbots2017/src/org/usfirst/frc/team1124/robot/commands/ParkAtFeeder.java
+++ Uberbots2017/src/org/usfirst/frc/team1124/robot/commands/ParkAtFeeder.java
@@ -4,28 +4,11 @@ import org.usfirst.frc.team1124.robot.Robot;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class ParkAtFeeder extends CommandGroup {
-	private boolean done = false;
-	
 	public ParkAtFeeder() {
-		requires(Robot.drive);
-		requires(Robot.gearDoor);
-		addParallel(new RaiseGearDoor());
-		addSequential(new Turn(Robot.drive.calcAngle()));
+		addParallel(new ToggleGearDoor(true));
+		addSequential(new AlignToWall());
 		addSequential(new DriveForward(Robot.drive.calcDist()));
 		addSequential(new TargetVisionTape());
-		addSequential(new WaitForGear());
-		addSequential(new Terminate(this));
-	}
-
-	public void initialize() {
-		done = false;
-	}
-
-	public boolean isFinished() {
-		return done;
-	}
-
-	public void cancel() {
-		done = true;
+		// If indicating ready with lights, add cmd to set here
 	}
 }
--- Uberbots2017/src/org/usfirst/frc/team1124/robot/commands/ToggleGearDoor.java
+++ Uberbots2017/src/org/usfirst/frc/team1124/robot/commands/ToggleGearDoor.java
@@ -1,12 +1,24 @@
 package org.usfirst.frc.team1124.robot.commands;
 import org.usfirst.frc.team1124.robot.Robot;
-
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class ToggleGearDoor extends Command {
-	
+	private Boolean raiseDoor = null;
+
+	public ToggleGearDoor(boolean raiseDoor) {
+    	this.raiseDoor = raiseDoor;
+    }
+
     public ToggleGearDoor() {}
-    protected void initialize() { Robot.gearDoor.toggle(); }
+    
+    protected void initialize() {
+    	if (raiseDoor != null) {
+    		Robot.gearDoor.set((raiseDoor) ? Value.kReverse : Value.kForward);
+    	} else {
+    		Robot.gearDoor.toggle();
+    	}
+    }
     protected void execute() {    }
     protected boolean isFinished() { return(true); }
     protected void end() {}
