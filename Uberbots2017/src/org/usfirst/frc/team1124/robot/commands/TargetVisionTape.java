ec9e7ddfbd28dad9f150457033a0a3fd2d55b6cc
Jennifer Weston <jweston@uberbots.org>
2017-02-23 09:15:12 -0500

    Updated strafing in vision tape code

--- Uberbots2017/src/org/usfirst/frc/team1124/robot/commands/TargetVisionTape.java
+++ Uberbots2017/src/org/usfirst/frc/team1124/robot/commands/TargetVisionTape.java
@@ -8,13 +8,10 @@ import org.usfirst.frc.team1124.vision.GripPipeline;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class TargetVisionTape extends Command {
+	private final int TOLERANCE = 2;
+	private final double MAX_SPEED = .4;
+
 	private boolean done = false;
-
-	public static final int CAMERA_EXPOSURE = 3;
-
-	private int tolerance = 2;
-	double buffer = .4;
-
 	private GripPipeline filter = null;
 	private int backupMode = 2;
 
@@ -22,21 +19,11 @@ public class TargetVisionTape extends Command {
 		requires(Robot.camera);
 		requires(Robot.drive);
 		filter = new GripPipeline();
-
-	}
-
-	protected void end() {
-		Robot.drive.frontLeft.set(0);
-		Robot.drive.frontRight.set(0);
-		Robot.drive.rearLeft.set(0);
-		Robot.drive.rearRight.set(0);
-		this.done = true;
-		Robot.drive.mode = backupMode;
 	}
 
 	protected void initialize() {
 		backupMode = Robot.drive.mode;
-		Robot.drive.mode = 3;
+		Robot.drive.mode = 2;
 		Robot.drive.lockAngle = Robot.drive.navx.getYaw();
 		done = false;
 	} // mec mode
@@ -59,31 +46,42 @@ public class TargetVisionTape extends Command {
 		// Find the middle
 		double mx = (lx + hx) / 2;
 		double dfif = mx - (Camera.CAMERA_RES_X / 2);
-		if (Math.abs(dfif) <= tolerance) {
+		if (Math.abs(dfif) <= TOLERANCE) {
 			done = true;
 			return;
-		} else if (Math.abs(dfif) >= tolerance) {
-			double strafe = dfif;
-			if (strafe <= -1.0) {
-				strafe = -1 * buffer;
-			} // left
-			else if (strafe >= 1.0) {
-				strafe = 1 * buffer;
-			} // right
-			if (Math.abs(dfif) > tolerance)
-				Robot.drive.run(strafe, 0);
-			else
-				done = true;
-			return;
 		}
+
+		// Figure out which direction to move in
+		double strafe = getSpeed(dfif);
+
+		// Split the speed into X and Y components
+		double strafeX = strafe * Math.cos(Robot.drive.lockAngle);
+		double strafeY = -1 * strafe * Math.sin(Robot.drive.lockAngle); // Multiply by -1 because lockAngle is at sin(-90) to direction
+		Robot.drive.run(strafeX, strafeY);
 	}
 
-	public void interrupted() {
+	private double getSpeed(double x) {
+		double speed = MAX_SPEED * x / Camera.CAMERA_RES_X * 0.05;
+		if (Math.abs(speed) > MAX_SPEED) {
+			speed = (speed < 0.0) ? -1 * MAX_SPEED : MAX_SPEED;
+		}
+
+		// If x is a positive number of pixels from center (i.e. to the right), move left (negative speed)
+		// If x is a negative number of pixels from center (i.e. to the left), move right (positive speed)
+		return -1 * speed;
+	}
+
+	protected void end() {
 		Robot.drive.frontLeft.set(0);
 		Robot.drive.frontRight.set(0);
 		Robot.drive.rearLeft.set(0);
 		Robot.drive.rearRight.set(0);
 		this.done = true;
+		Robot.drive.mode = backupMode;
+	}
+
+	public void interrupted() {
+		end();
 	}
 
 	@Override
