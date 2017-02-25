4462347d0d4ff2fd2457fdc8231ea597a6f5b39f
mikobb221 <m.benvenuto212@gmail.com>
2017-02-25 09:58:12 -0500

    Real Comments
    Messup with the last commit deleting the buttons

--- Uberbots2017/src/org/usfirst/frc/team1124/robot/OI.java
+++ Uberbots2017/src/org/usfirst/frc/team1124/robot/OI.java
@@ -19,6 +19,13 @@ public class OI {
 	// The Controllers
 	public static Joystick stick = new Joystick(0);
 	public static Joystick stickTwo = new Joystick(1);
+	Button pressToWin = new JoystickButton(stick, 1);         //A, D1
+	Button toggleGearDoor = new JoystickButton(stick, 2);     //B, D1
+	Button toggleClimbDoor = new JoystickButton(stickTwo, 5); //LB, D2
+	Button climbButton = new JoystickButton(stickTwo, 6);     //RB, D2
+	Button climbReverse = new JoystickButton(stickTwo, 8);    //Start, D2
+	Button visionTest = new JoystickButton(stick, 3);         //X, D1
+	Button turnTest = new JoystickButton(stick, 4);           //Y, D1
 
 	public OI() {
 		pressToWin.whileHeld(new PressToWin());
